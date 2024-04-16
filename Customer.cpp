3f7a452fec34c3409134fbdaee3139a65b924366
Saikmat <saikmat@Outlook.com>
2024-04-16 19:33:30 -0400

    Fixed hardcoded values and output not showing correctly

--- Customer.cpp
+++ Customer.cpp
@@ -1,109 +1,62 @@
 // Copyright 2023 - Howard Community College All rights reserved; Unauthorized duplication prohibited.
 
-#include <iomanip>
 #include <ostream>
+#include <iomanip>
 #include "Customer.h"
 #include "Cellphone.h"
 
-using namespace std;
-
-/*
- * Constructs a customer object given the name, number of phones purchased, and the list of cellphones in their cart
- */
-Customer::Customer(const string &customerName, int numberOfPhonesPurchased, Cellphone** cellPhones){
+Customer::Customer(const std::string &customerName, int numberOfPhonesPurchased, Cellphone** cellPhones){
     this->customerName = customerName;
     this->numberOfPhonesPurchased = numberOfPhonesPurchased;
-    this->cellPhones = new Cellphone[numberOfPhonesPurchased];
     for (int i = 0; i < numberOfPhonesPurchased; ++i) {
-        this->cellPhones[i] = *cellPhones[i];
+        this->cellPhones[i] = cellPhones[i];
     }
     findTotalCost();
 }
 
-/*
- * Constructs a default customer with one cellphone in their cart
- */
 Customer::Customer() {
     customerName = "none";
     costOfPurchase = 0.0;
     numberOfPhonesPurchased = 1;
-    cellPhones[0] = *new Cellphone();
+    cellPhones[0] = new Cellphone();
 }
 
-/*
- * Returns the customer's name
- */
-const string &Customer::getCustomerName() const {
+const std::string &Customer::getCustomerName() const {
     return customerName;
 }
 
-/*
- * Sets the customer's name
- */
-void Customer::setCustomerName(const string &customerName) {
+void Customer::setCustomerName(const std::string &customerName) {
     Customer::customerName = customerName;
 }
 
-/*
- * Returns the number of phones that were purchased
- */
 int Customer::getNumberOfPhonesPurchased() const {
     return numberOfPhonesPurchased;
 }
 
-/*
- * Sets the number of phones to be purchased
- */
 void Customer::setNumberOfPhonesPurchased(int numberOfPhonesPurchased) {
     Customer::numberOfPhonesPurchased = numberOfPhonesPurchased;
     findTotalCost();
 }
 
-/*
- * Returns the total cost of purchase
- */
 double Customer::getCostOfPurchase() const {
     return costOfPurchase;
 }
 
-/*
- * Returns the array of cellphones that were purchased
- */
 Cellphone* Customer::getCellPhones() const {
-    return cellPhones;
+    return *cellPhones;
 }
 
-/*
- * Find the total cost including taxes of phones for purchase
- */
 void Customer::findTotalCost() {
     costOfPurchase = COST * numberOfPhonesPurchased * SALES_TAX;
+    
 }
 
-/*
- * Overrides the ostream operator for Customer objects for ease of printing
- * Returns the full stream with receipt informationm
- */
-ostream &operator<<(ostream &ostream1, Customer *c) {
-    c->findTotalCost();
-    ostream1 << setprecision(2);
-    ostream1 << "Receipt"
-            << "\nCustomer name:    " << c->getCustomerName()
-            << "\nNumber of Phones: " << c->getNumberOfPhonesPurchased()
-            << "\nTotal amount due: $" << c->getCostOfPurchase()
-            << "\n\n"
-            << "Phone(s) ID and number:" << endl;
-    for (int i = 0; i < c->getNumberOfPhonesPurchased(); ++i) {
-        Cellphone* phone = &c->getCellPhones()[i];
-        ostream1 << "ID Number: " << phone->getCellphoneIdNumber() << "\t\t\t" << "Phone Number: " << phone->getCellphoneNumber() << endl;
-    }
-    return ostream1;
-}
-
-/*
- * Destroys the array of cellphones
- */
-Customer::~Customer() {
-    delete[] cellPhones;
+std::ostream &operator<<(std::ostream &ostream1, Customer *c) {
+    int costLength = 5 + (c->getCostOfPurchase() > 1000);
+    return ostream1 << std::setprecision(costLength)
+                    << "Receipt"
+                    << "\nCustomer name: " << c->getCustomerName()
+                    << "\nNumber of Phones: " << c->getNumberOfPhonesPurchased()
+                    << "\nTotal amount due: $" << c->getCostOfPurchase() << std::endl;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-/*
- * Copyright ©2024 – Howard Community College All rights reserved; Unauthorized duplication prohibited.
- * Name: Sai Matukumalli
- * Program Name: Cellphone checkout software
- * Program Description: This program maintains a checkout system for cellphones, tracking how many people have a phone, how many people are customers, and who has what phones
- */
-
-
 #include <iostream>
 #include <vector>
 #include <stack>
@@ -16,42 +8,39 @@
 
 using namespace std;
 
-void createStack(stack<Cellphone, vector<Cellphone>>&);
+void createStack(stack<Cellphone*, vector<Cellphone*>>&);
 
-void buyPhone(stack<Cellphone, vector<Cellphone>>& cellphones, deque<Customer*>&);
+void buyPhone(stack<Cellphone*, vector<Cellphone*>>& cellphones, deque<Customer*>&);
 
 void checkout(deque<Customer*> &customers);
 
-void printCopyright();
-
-int validateInput(const string &menu, const stack<Cellphone, vector<Cellphone>> &cellphones, int input);
-const int DEFAULT = 0;
-const int BUY_PHONE = 1;
-const int CUSTOMER_CHECKOUT = 2;
-const int QUIT = 3;
-
-/*
- * Runs the program
- */
 int main() {
-    printCopyright();
-
-    const string menu = "\n1. Buy Phone\n"
+    const int BUY_PHONE = 1;
+    const int CUSTOMER_CHECKOUT = 2;
+    const int QUIT = 3;
+    const string menu = "1. Buy Phone\n"
                         "2. Check out\n"
                         "3. Quit\n";
     
-    stack<Cellphone, vector<Cellphone>> cellphones;
+    stack<Cellphone*, vector<Cellphone*>> cellphones;
     deque<Customer*> customers;
     createStack(cellphones);
     
-    int input = DEFAULT;
-
-    while (input != QUIT){
+    int input;
+    do {
         cout << menu;
         cout << "Enter a menu choice";
         cin >> input;
-        input = validateInput(menu, cellphones, input);
-        switch (input) { // NOLINT(*-multiway-paths-covered)
+        while (input < BUY_PHONE || input > QUIT) {
+            if (cellphones.empty()) { //prevents case 1 if stack is empty
+                cout << "Invalid Choice! Enter a number between " << CUSTOMER_CHECKOUT << " and " << QUIT;
+                cin >> input;
+            } else {
+                cout << "Invalid choice! Enter a number between " << BUY_PHONE << " and " << QUIT;
+                cin >> input;
+            }
+        }
+        switch (input) {
             case BUY_PHONE: {
                 buyPhone(cellphones, customers);
                 break;
@@ -60,101 +49,56 @@ int main() {
                 checkout(customers);
                 break;
             }
+            default:
             case QUIT: {
                 if(!customers.empty()){
-                    cout << "There are still " << customers.size() << " customers in the queue" << endl;
-                    input = DEFAULT;
+                    cout << "There are still customers in the queue";
                     break;
                 }
                 cout << "Thank you for using the phone purchase software";
                 return 0;
             }
         }
-    }
+    } while (input != QUIT);
     cout << "Thank you for using the phone purchase software";
     return 0;
 }
 
-/*
- * Checks if the menu input is valid and makes sure that the user enters a value that is actually contained in the inputs
- * Also prevents the user from entering the buy menu when the stack is empty
- */
-int validateInput(const string &menu, const stack<Cellphone, vector<Cellphone>> &cellphones, int input) {
-    while (input < BUY_PHONE || input > QUIT) {
-        cout << "Invalid choice! Enter a number between " << BUY_PHONE << " and " << QUIT;
-        cout << menu;
-        cin >> input;
-    }
-    while(input == BUY_PHONE && cellphones.empty()){
-        cout << "There are no more phones for purchase! Enter a number between " << CUSTOMER_CHECKOUT << " and " << QUIT;
-        cout << menu;
-        cin >> input;
-    }
-
-    return input;
-}
-
-/*
- * Prints the receipt of the customer at the front of the parameter deque
- * No return since the print is done in the menu
- */
 void checkout(deque<Customer*>& customers) {
     if(customers.empty()){
         cout << "Error, there are no customers in the queue";
         return;
     }
-    Customer* c = customers.front();
-    customers.pop_front();
+    Customer* c = customers.back();
     cout << c;
+    customers.pop_back();
     delete c;
 }
 
-/*
- * Allows the customer to buy a phone
- * Takes in a stack of cellphones that are available and the customer deque to push customers onto
- * Pops a user entered number phones off the stack and into the customer's "shopping cart" so they can check out
- * No return, all data is modified in the method itself
- */
-void buyPhone(stack<Cellphone, vector<Cellphone>>& cellphones, deque<Customer*>& customers){
+void buyPhone(stack<Cellphone*, vector<Cellphone*>>& cellphones, deque<Customer*>& customers){
+    const int MAX_PHONES = 6;
     string customerName;
-    int numberOfPhonesPurchased = 0;
-    cout << "Enter the name of the customer: ";
-    cin.ignore();
-    getline(cin, customerName);
-    cout << "Enter the number of phones: ";
+    int numberOfPhonesPurchased;
+    cout << "Enter tha name of the customer: ";
+    cin >> customerName;
+    cout << "Enter the number of phones";
     cin >> numberOfPhonesPurchased;
-    while(numberOfPhonesPurchased < 1 || numberOfPhonesPurchased > 6 || numberOfPhonesPurchased > cellphones.size()){
-        cout << "Not Enough Phones! Enter a number between 1 and " << min((unsigned long long) 6, cellphones.size());
+    while(numberOfPhonesPurchased < 1 || numberOfPhonesPurchased > MAX_PHONES || numberOfPhonesPurchased > cellphones.size()){
+        cout << "Invalid input! Enter a number between 1 and " << min((unsigned long long) MAX_PHONES, cellphones.size());
         cin >> numberOfPhonesPurchased;
     }
-    Cellphone* phonesPurchased[numberOfPhonesPurchased];
-
+    Cellphone** phonesPurchased = new Cellphone*[numberOfPhonesPurchased];
     for (int i = 0; i < numberOfPhonesPurchased; ++i) {
-        phonesPurchased[i] = &cellphones.top();
+        phonesPurchased[i] = cellphones.top();
         cellphones.pop();
     }
     customers.push_back(new Customer(customerName, numberOfPhonesPurchased, phonesPurchased));
 }
 
-/*
- * Reads the text file of all the cellphones and creates a stack of cellphone objects given the ID and phone number
- * No return, the stack is modified in place
- */
-void createStack(stack <Cellphone, vector<Cellphone>>& cellphones){
-    ifstream* ifstream1 = new ifstream(R"(C:\Users\SaiKM\CLionProjects\Lab9\Cellphone.txt)", ios::in);
-    while(!ifstream1->eof()){
-        string phoneID, phoneNumber;
-        getline(*ifstream1, phoneID);
-        getline(*ifstream1, phoneNumber);
-        cellphones.emplace(phoneID, phoneNumber);
+void createStack(stack <Cellphone*, vector<Cellphone*>>& cellphones){
+    fstream* fstream1 = new fstream(R"(C:\Users\SaiKM\CLionProjects\Lab9\Cellphone.cpp)", ios::in);
+    while(!fstream1->eof()){
+        cellphones.push(reinterpret_cast<Cellphone*>(fstream1->get()));
     }
-    ifstream1->close();
-    delete ifstream1;
-}
-
-/*
- * Prints the HCC copyright
- */
-void printCopyright(){
-    cout << "\n\nCopyright ©2024 – Howard Community College All rights reserved; Unauthorized duplication prohibited.\n\n\n\n";
+    fstream1->close();
 }
